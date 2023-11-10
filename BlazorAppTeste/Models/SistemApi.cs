05cdb3ce9707fa4b065405e161eefc7d3828c000
MauricioReis04 <107776890+mauricioreiss@users.noreply.github.com>
2023-11-09 23:40:57 -0300

    commit initial teste
    commit teste

---
+++ BlazorAppTeste/Models/SistemApi.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAppTeste.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SistemApi : ControllerBase
+    {
+        public int IdSistema { get; set; } 
+        public string Descricao { get; set; } 
+    }
+}
